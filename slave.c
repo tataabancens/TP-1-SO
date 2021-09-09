42c74b68b7827bde2b87ee14a492a121f4a87f83
Tataabancens <tataabancens@gmail.com>
2021-09-09 11:10:43 -0300

    Merge linting

--- slave.c
+++ slave.c
@@ -2,22 +2,23 @@
 #include <string.h>
 #include <unistd.h>
 
-int main (int argc, char* argv[]){
+int main(int argc, char* argv[]) {
+    
     char command[500];
     char output[500];
     sprintf(command, "%s %s | grep -o -e \"Number of.*[0-9]\\+\" -e \"CPU time.*\" -e \".*SATISFIABLE\"", "minisat", argv[1]);
+    
     FILE *stream;
-    if((stream = popen(command, "r")) == NULL)
-    {
+    if ((stream = popen(command, "r")) == NULL) {
         printf("Error en popen");
     }
+    
     int dim = fread(output, sizeof(char), 499, stream);
-
     output[dim] = 0;
 
     printf("PID: %d Filename: %s\n%s", getpid(), argv[1], output);
 
-    if(pclose(stream) == -1){
+    if (pclose(stream) == -1) {
         printf("Error en pclose");
     }
     return 0;
